6bff54936e86ab513b5987ed464982f0628d5135
BRUNO NUNES <05563115070@senacrs.edu.br>
2025-05-15 13:40:43 -0300

    feat: prisma repository for profile pictures

---
+++ src/repositories/prisma/prisma-profile-picture-repository.ts
@@ -0,0 +1,41 @@
+import { Prisma } from "../../generated/prisma";
+import { prisma } from "../../lib/prisma";
+import { IProfilePicturesRepository } from "../profile-picture-repository";
+
+export class PrismaProfilePictureRepository implements IProfilePicturesRepository {
+    async list() {
+        return await prisma.profilePicture.findMany();
+    }
+
+    async findById(id: string) {
+        return prisma.profilePicture.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
+
+    async create(data: Prisma.ProfilePictureCreateInput) {
+        const profilePicture = await prisma.profilePicture.create({
+            data,
+        });
+
+        return {
+            id: profilePicture.id,
+            url: profilePicture.url,
+        };
+    }
+    
+    async delete(id: string) {
+        const profilePicture = await prisma.profilePicture.delete({
+            where: {
+                id,
+            },
+        });
+
+        return {
+            id: profilePicture.id,
+            url: profilePicture.url,
+        };
+    }
+}
