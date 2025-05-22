c2dbebffd69249f3da20863d39fe17c0e38f4d1c
BRUNO NUNES <05563115070@senacrs.edu.br>
2025-05-22 17:20:46 -0300

    fix: swagger documentation

--- src/routes/choice-routes.ts
+++ src/routes/choice-routes.ts
@@ -5,7 +5,7 @@ import { verifyUserRoleMiddleware } from "../middlewares/verify-user-role";
 const choiceRouter = Router();
 
 choiceRouter.get("/choices/:id", getChoiceByIdController);
-choiceRouter.get("/choices/list/exerciseId", listChoicesByExerciseController);
+choiceRouter.get("/choices/list/:exerciseId", listChoicesByExerciseController);
 choiceRouter.post("/choices", verifyUserRoleMiddleware, createChoiceController);
 choiceRouter.put("/choices/:id", verifyUserRoleMiddleware, updateChoiceController);
 choiceRouter.delete("/choices/:id", verifyUserRoleMiddleware, deleteChoiceController);
