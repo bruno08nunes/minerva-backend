383de912755e63bf1b681d952b154be5b92e777a
bruno08nunes <brunonunes.sl08@gmail.com>
2025-08-02 19:28:36 -0300

    feat: add new topics and icons

--- src/seed/icon-seed.ts
+++ src/seed/icon-seed.ts
@@ -4,10 +4,6 @@ export async function createIcons() {
     return prisma.icon.createMany({
         data: [
             {
-                id: "9897d6f2-fb41-4554-b22f-d251f9941fa3",
-                url: "variable.webp",
-            },
-            {
                 id: "6668bcde-d0bf-4e7e-8a33-a1a870e5051e",
                 url: "rpg.jpg",
             },
@@ -23,6 +19,34 @@ export async function createIcons() {
                 id: "a0771cdd-ea68-4737-bb30-401fe7da4b98",
                 url: "winner-achievement.png",
             },
+            {
+                id: "030f0d8a-ee82-41a0-8cb1-b889e621f3e8",
+                url: "variables.png"
+            },
+            {
+                id: "5b9cee42-3800-4402-b84d-e9c1b155cd56",
+                url: "operators.png"
+            },
+            {
+                id: "2702e970-e644-477d-b122-997b7a9eb3f0",
+                url: "conditionals.png"
+            },
+            {
+                id: "6f57862d-63f3-4188-a144-daf4bd55b8cf",
+                url: "loops.png"
+            },
+            {
+                id: "af475526-d808-4e1a-8f57-ca2635a6499e",
+                url: "functions.png"
+            },
+            {
+                id: "3eb5016e-16be-4d96-a14d-24942d5d9484",
+                url: "arrays.png"
+            },
+            {
+                id: "81998ea7-52a7-416d-9e09-6585e52033b0",
+                url: "objects.png"
+            }
         ],
     });
 }
--- src/seed/topic-seed.ts
+++ src/seed/topic-seed.ts
@@ -7,9 +7,51 @@ export async function createTopics() {
                 id: "e58681e3-8305-4787-b63f-9e58b07c52a8",
                 name: "Variáveis",
                 description: "Aprenda sobre variáveis, tipos de dados e como armazenar informações em seu código.",
-                iconId: "9897d6f2-fb41-4554-b22f-d251f9941fa3",
+                iconId: "030f0d8a-ee82-41a0-8cb1-b889e621f3e8",
                 slug: "variaveis",
-            }
+            },
+            {
+                id: "218492d4-2a81-4b5a-a0fe-1fd644ee38be",
+                name: "Operadores",
+                description: "Aprenda sobre operadores aritméticos, lógicos e relacionais.",
+                iconId: "5b9cee42-3800-4402-b84d-e9c1b155cd56",
+                slug: "operadores",
+            },
+            {
+                id: "7e3748c5-cb38-4304-9319-ba805dd13aa4",
+                name: "Condicionais",
+                description: "Aprenda sobre como mudar o percurso do código.",
+                iconId: "2702e970-e644-477d-b122-997b7a9eb3f0",
+                slug: "condicionais",
+            },
+            {
+                id: "d20a6380-9ee1-4a41-81fe-758133aaf35c",
+                name: "Loops",
+                description: "Aprenda sobre como repetir uma ação no código.",
+                iconId: "6f57862d-63f3-4188-a144-daf4bd55b8cf",
+                slug: "loops",
+            },
+            {
+                id: "b6101fef-e2c7-4c68-91dc-d47f10ba497c",
+                name: "Funções",
+                description: "Aprenda sobre como separar ações logicamente.",
+                iconId: "af475526-d808-4e1a-8f57-ca2635a6499e",
+                slug: "funcoes",
+            },
+            {
+                id: "36e04352-0d70-4198-8dfd-0e064e0d7e78",
+                name: "Arrays",
+                description: "Aprenda sobre como agrupar valores.",
+                iconId: "3eb5016e-16be-4d96-a14d-24942d5d9484",
+                slug: "arrays",
+            },
+            {
+                id: "205bf383-5b91-4502-a489-f9dd250fdcc1",
+                name: "Objetos",
+                description: "Aprenda sobre como agrupar valores.",
+                iconId: "81998ea7-52a7-416d-9e09-6585e52033b0",
+                slug: "objetos",
+            },
         ]
     });
 }
