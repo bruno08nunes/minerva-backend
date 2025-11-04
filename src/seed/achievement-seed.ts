12d45132932cb7efbb750c61f5a6f4647c6c653e
BRUNO NUNES <05563115070@senacrs.edu.br>
2025-11-04 17:45:33 -0300

    feat: add achievement type and amount

--- src/seed/achievement-seed.ts
+++ src/seed/achievement-seed.ts
@@ -6,20 +6,26 @@ export const achievements = [
         name: "Campeão!",
         description: "Fique em primeiro lugar no ranking.",
         iconId: "a0771cdd-ea68-4737-bb30-401fe7da4b98",
+        type: "PROGRESS",
+        amount: 10
     },
     {
         id: "b2c3d4e5-f6g7-h8i9-j0k1-l2m3n4o5p6q7",
         name: "Mais que Amigos",
         description: "Siga um usuário que lhe segue.",
         iconId: "11558194-e95f-45d0-a3af-aa8058ac2b37",
+        type: "PROGRESS",
+        amount: 10
     },
     {
         id: "c3d4e5f6-g7h8-i9j0-k1l2-m3n4o5p6q7r8",
         name: "Desbravador de Exercícios",
         description: "Complete 10 exercícios de programação.",
         iconId: "35b91640-7b4e-4988-a9f9-8027b4280f5b",
+        type: "PROGRESS",
+        amount: 10
     },
-];
+] as const;
 
 export async function createAchievements() {
     return prisma.achievement.createMany({
@@ -28,6 +34,8 @@ export async function createAchievements() {
             name: achievement.name,
             description: achievement.description,
             iconId: achievement.iconId,
+            amount: achievement.amount,
+            type: achievement.type
         })),
     });
 }
